76f0cc58857771a40e81cca5807b1338e756681e
Tadeáš Kapina <tadeas.kapina4@gmail.com>
2025-11-05 18:13:31 +0100

    pridané odosielanie mailov

--- src/main/java/org/upece/granko/olvmat/controller/ReservationController.java
+++ src/main/java/org/upece/granko/olvmat/controller/ReservationController.java
@@ -7,12 +7,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.upece.granko.olvmat.entity.TicketEntity;
 import org.upece.granko.olvmat.model.ReservationForm;
 import org.upece.granko.olvmat.repository.TicketRepository;
+import org.upece.granko.olvmat.service.EmailService;
 
 @Controller
 @RequiredArgsConstructor
 public class ReservationController {
 
     private final TicketRepository ticketRepository;
+    private final EmailService emailService;
 
     @GetMapping("")
     public String rezervacia() {
@@ -23,6 +25,7 @@ public class ReservationController {
     public String odosliRezervaciu(ReservationForm form) {
 
         ticketRepository.save(new TicketEntity(form.getName(), form.getEmail()));
+        emailService.sendMail(form.getEmail(), "Predaj lístka", "Ďakujeme " + form.getName());
 
         return "rezervacia";
     }
---
+++ src/main/java/org/upece/granko/olvmat/service/EmailService.java
@@ -0,0 +1,26 @@
+package org.upece.granko.olvmat.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+
+    @Value("${mail.from}")
+    private String from;
+
+    public void sendMail(String to, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(from);
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+        mailSender.send(message);
+    }
+}
