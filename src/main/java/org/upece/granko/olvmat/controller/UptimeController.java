5a4977d720772652bfcc99eda3ddfb261431201f
Tadeáš Kapina <tadeas.kapina4@gmail.com>
2025-11-10 12:07:49 +0100

    remove /test endpoint

--- src/main/java/org/upece/granko/olvmat/controller/UptimeController.java
+++ src/main/java/org/upece/granko/olvmat/controller/UptimeController.java
@@ -3,26 +3,15 @@ package org.upece.granko.olvmat.controller;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.upece.granko.olvmat.entity.TicketEntity;
-import org.upece.granko.olvmat.repository.TicketRepository;
 
 import java.time.LocalDate;
-import java.util.List;
 
 @RestController
 @AllArgsConstructor
 public class UptimeController {
-
-    private final TicketRepository ticketRepository;
-
     @GetMapping("healt")
     public UptimeStatus getUp() {
         return new UptimeStatus("UP", LocalDate.now());
-    }
-
-    @GetMapping("test")
-    public List<TicketEntity> getTest() {
-        return ticketRepository.findAll();
     }
 
     public record UptimeStatus(
