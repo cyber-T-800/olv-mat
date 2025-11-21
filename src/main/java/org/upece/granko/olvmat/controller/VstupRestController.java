7f58ec7043eb0b2e51772a8d35eb117b8e8fff9f
Tadeáš Kapina <tadeas.kapina4@gmail.com>
2025-11-21 19:25:20 +0100

    fix volania endpointu

--- src/main/java/org/upece/granko/olvmat/controller/VstupRestController.java
+++ src/main/java/org/upece/granko/olvmat/controller/VstupRestController.java
@@ -18,19 +18,23 @@ public class VstupRestController {
     private final TicketRepository ticketRepository;
 
     @GetMapping("{id}")
-    public Ticket nacitajListok(@PathVariable UUID id){
-        TicketEntity entity =  ticketRepository.findById(id).orElseThrow();
+    public Ticket nacitajListok(@PathVariable UUID id, HttpServletResponse response) {
+        TicketEntity entity = ticketRepository.findById(id).orElse(null);
+        if (entity == null) {
+            response.setStatus(404);
+            return null;
+        }
         return new Ticket(entity.getId(), entity.getEmail(), entity.getMeno(), entity.getTypListka(), entity.getStav());
     }
 
     @PutMapping("{id}/potvrd")
-    public void potvrdListok(@PathVariable UUID id, HttpServletResponse response){
-        TicketEntity entity =  ticketRepository.findById(id).orElse(null);
-        if(entity == null){
+    public void potvrdListok(@PathVariable UUID id, HttpServletResponse response) {
+        TicketEntity entity = ticketRepository.findById(id).orElse(null);
+        if (entity == null) {
             response.setStatus(404);
             return;
         }
-        if(entity.getStav() != StavRezervacieEnum.ZAPLATENY){
+        if (entity.getStav() != StavRezervacieEnum.ZAPLATENY) {
             response.setStatus(400);
         }
         entity.setStav(StavRezervacieEnum.POUZITY);
