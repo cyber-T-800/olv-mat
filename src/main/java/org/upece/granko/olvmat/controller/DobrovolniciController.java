6273deab50aecf153a9d299947eaa259031e8d19
Tadeáš Kapina <tadeas.kapina4@gmail.com>
2026-03-26 15:30:54 +0100

    pridany atribut bookings pre dobrovolnicku stranku

--- src/main/java/org/upece/granko/olvmat/controller/DobrovolniciController.java
+++ src/main/java/org/upece/granko/olvmat/controller/DobrovolniciController.java
@@ -1,13 +1,17 @@
 package org.upece.granko.olvmat.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
 
 @Controller
 public class DobrovolniciController {
 
     @GetMapping("dobrovolnici")
-    public String getDobrovolnici(){
+    public String getDobrovolnici(ModelMap model){
+        model.addAttribute("bookings", new ArrayList<String>());
         return "dobrovolnici";
     }
 }
